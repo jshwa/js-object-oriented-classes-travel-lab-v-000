e9b749e5f614d4b6418dc19ec01e5d7d17f8df2b
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 04:20:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ class Route {
 
   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-    let horiz = eastWest.slice(this.beginningLocation.horizontal, this.endingLocation.horizontal).length
-    debugger
     return (eastWest.slice(this.beginningLocation.horizontal, this.endingLocation.horizontal).length) + (this.endingLocation.vertical - this.beginningLocation.vertical)
   }
 }
