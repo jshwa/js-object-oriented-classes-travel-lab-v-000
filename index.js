70dd032e91c193984659f2f1f7301e6a5148c743
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 04:09:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class Route {
     this.endingLocation = endingLocation;
   }
 
+  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 
   blocksTravelled() {
 
