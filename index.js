3ae3b8061c0032380de108899c10f7e7ed515d64
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 04:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+class Driver {
+  constructor (name, day){
+    this.name = name;
+    this.day = day;
+  }
 
+  startDate(){
+    return new Date(this.day)
+  }
+
+  yearsExperienceFromBeginningOf(year){
+    
+  }
+}
